a381452a07a9ca417459f2aced63728c88e64959
André <cardenasvale04@gmail.com>
2021-08-09 23:00:36 -0600

    agregado opciones menu

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import time
+from io import open
 
 a = True
 
@@ -6,13 +7,28 @@ while a:
     print("Seccione una opcion")
     eleccion = int(input("1. Cargar archivo \n2. Procesar archivo \n3. Escribir archivo de salida \n4. Mostrar datos del estudiante \n5. Generar grafica \n6. Salir \n"))
     if eleccion == 1:
-        print("asdf")
+        print(" ...Abriendo el archivo ")
+
+        archivo_nuevo = input("Ingrese la ruta del archivo: ")
+        cargar_archivo = open(archivo_nuevo, "r")
+        print(cargar_archivo.readlines()) 
+        cargar_archivo.close() 
+
     elif eleccion == 2:
-        print(eleccion)
+        print(" ")
     elif eleccion == 3:
-        print(eleccion)
+        print(" ")
     elif eleccion == 4:
-        print("----------------------------------------------------------------")
-        print("---Bienvenido---")
-        print("Hola")
-        print("ya")
+        print("*----------------------------------------------------------------*")
+        print("--- André Valentín Méndez Cárdenas ---")
+        print("--- Carnet: 201801399 ---")
+        print("--- Introducción a la Programación y Computación 2 sección 'A' ---")
+        print("--- Ingenieria en Ciencias y Sistemas ---")
+        print("--- 4to Semestre ---")
+        print("*----------------------------------------------------------------*")
+    elif eleccion == 5:
+        print(eleccion)
+    elif eleccion == 6:
+        print("Saliendo del programa...")
+        time.sleep(3)
+        a = False
