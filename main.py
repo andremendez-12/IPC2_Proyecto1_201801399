b41bc87acb210f2acdbaea691a34e23118a19e13
André <cardenasvale04@gmail.com>
2021-08-09 22:04:04 -0600

    codigo subido menu

--- main.py
+++ main.py
@@ -1,2 +1,16 @@
+import time
+
 a = True
-opcion = True
+
+while a:
+    print("Seccione una opcion")
+    eleccion = int(input("1. Cargar archivo \n2. Procesar archivo \n3. Escribir archivo de salida \n4. Mostrar datos del estudiante \n5. Generar grafica \n6. Salir \n"))
+    if eleccion == 1:
+        print("asdf")
+    elif eleccion == 2:
+        print(eleccion)
+    elif eleccion == 3:
+        print(eleccion)
+    elif eleccion == 4:
+        print("----------------------------------------------------------------")
+        print("---Bienvenido---")
