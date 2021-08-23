2e7ac94c368ada1412fabe38382840a6d99c4808
André <cardenasvale04@gmail.com>
2021-08-22 20:51:35 -0600

    agregado clase lista

---
+++ lista.py
@@ -0,0 +1,5 @@
+class lista: 
+    def __init__(self, info = None, siguiente = None, anterior = None):
+        self.info = info
+        self.siguiente = siguiente
+        self.anterior = anterior
